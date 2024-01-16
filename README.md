11f5dd70da6e8a652039bf393a762b90e8cd48a1
dlbueno <142638660+dlbueno@users.noreply.github.com>
2024-01-16 11:16:19 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# joy-of-coding-internship-solo-project-dorabueno_26116
+# joy-of-coding-internship-solo-project-dorabueno_26116
+# joy-of-coding-internship-solo-project-dorabueno_26116
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html','/src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      colors:{
+        'twitter-blue': '#1DA1F2',
+        'red':'#e71414',
+        'white':'#faf5f5'
+      
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
